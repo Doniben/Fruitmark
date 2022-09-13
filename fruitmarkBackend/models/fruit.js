c864cf35a04500328ec02b951fb393d7dfc8cc55
Doniben <doniben@esperanto.co>
2022-09-13 00:03:44 -0500

    updated fields in fruit schema

--- fruitmarkBackend/models/fruit.js
+++ fruitmarkBackend/models/fruit.js
@@ -1,12 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const fruitSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  city: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "City",
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-exports.Fruit = mongoose.model('Fruit', fruitSchema);
+exports.Fruit = mongoose.model("Fruit", fruitSchema);
