f6e9c9eb55b5bfe3be8bfd696c41466a200767e1
Doniben <doniben@esperanto.co>
2022-09-14 23:36:35 -0500

    Add Context and Redux for Auth and fruittransfer

--- fruitmarkRNFront/App.js
+++ fruitmarkRNFront/App.js
@@ -3,41 +3,33 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider, extendTheme }  from 'native-base';
-//! import Toast from "react-native-toast-message";
+import Toast from "react-native-toast-message";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./Redux/store";
+
+// Context API
+import Auth from "./Context/store/Auth";
 
 	// Navigatiors
   import Main from "./Navigators/Main";
-
-  // Screens
-  // import Header from "./Shared/Header";
   
   LogBox.ignoreAllLogs(true);
 
-/*   const newColorTheme =  {
-    brand: {
-      900: '#5B8DF6',
-      800: '#ffffff',
-      700: '#cccccc'
-    }
-  };
-
-  const newColor = () => {
-
-  }
-
-const theme = extendTheme({
-  colors: newColorTheme,
-}); */
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        {/* <Header /> */}
-        <Main />
-        {/* <Toast ref={(ref) => Toast.setRef(ref)} /> */}
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <Auth>
+      <Provider store={store}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Main />
+          <Toast ref={(ref) => Toast.setRef(ref)} />
+        </NavigationContainer>
+      </NativeBaseProvider>
+      </Provider>
+    </Auth>
   );
 }
 
