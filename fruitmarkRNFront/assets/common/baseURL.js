d6bb04913c48f436a36c6599c8422cc7d828208c
Doniben <doniben@esperanto.co>
2022-09-14 23:41:01 -0500

    Util componets

--- fruitmarkRNFront/assets/common/baseURL.js
+++ fruitmarkRNFront/assets/common/baseURL.js
@@ -4,8 +4,10 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 { Platform.OS == 'android'
-  ? baseURL = 'https://fruitmark-production.up.railway.app/api/v1/'
+  ? baseURL = 'https://fruitmark-production.up.railway.app/'
   : baseURL = 'https://fruitmark.vercel.app/api/v1/'
 }
-
+// https://fruitmark-production.up.railway.app/
+// http://192.168.1.132:5100/api/v1/
+// https://fruitmark.vercel.app/api/v1/
 export default baseURL;
