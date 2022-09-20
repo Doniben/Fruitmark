412cef7f6c7df9060de547666e962aeeb758d0a8
Doniben <doniben@esperanto.co>
2022-09-20 11:15:30 -0500

    Updated styles in global form

--- fruitmarkRNFront/Shared/Form/FormContainer.js
+++ fruitmarkRNFront/Shared/Form/FormContainer.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { ScrollView, Dimensions, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { ScrollView, Dimensions, StyleSheet, Text } from "react-native";
 
-var { width } = Dimensions.get('window');
+var { width } = Dimensions.get("window");
 
 const FormContainer = (props) => {
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Text style={styles.title}>{props.title}</Text>
-            {props.children}
-        </ScrollView>
-    )
-}
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>{props.title}</Text>
+      {props.children}
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 30,
-        marginBottom: 400,
-        width: width,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 30,
-    }
-})
+  container: {
+    marginTop: -80,
+    marginBottom: 400,
+    width: width,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 30,
+    color: "rgba(0,164,109,1)"
+  },
+});
 
 export default FormContainer;
