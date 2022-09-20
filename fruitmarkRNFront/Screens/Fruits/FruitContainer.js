651757a57250d3cbbfcee4e26dd99b46407688ad
Doniben <doniben@esperanto.co>
2022-09-20 11:13:21 -0500

    Updated styles in main fruits

--- fruitmarkRNFront/Screens/Fruits/FruitContainer.js
+++ fruitmarkRNFront/Screens/Fruits/FruitContainer.js
@@ -7,10 +7,10 @@ import {
   ScrollView,
   Dimensions,
   SafeAreaView,
-  Image
+  Image,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { Container, Header, Icon, Item, Input, Text } from "native-base";
+import { Text } from "native-base";
 import { useFocusEffect } from "@react-navigation/native";
 import baseURL from "../../assets/common/baseURL";
 import axios from "axios";
@@ -70,21 +70,6 @@ const FruitContainer = (props) => {
     }, [])
   );
 
-  // Fruit Methods
-  const searchFruit = (text) => {
-    setFruitsFiltered(
-      fruits.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
-    );
-  };
-
-  const openList = () => {
-    setFocus(true);
-  };
-
-  const onBlur = () => {
-    setFocus(false);
-  };
-
   // Cities
   const changeCtg = (ctg) => {
     {
@@ -102,74 +87,74 @@ const FruitContainer = (props) => {
   return (
     <>
       <SafeAreaView style={styles.header}>
-      <View
-        style={{
-          backgroundColor: "#00a46c",
-          height: "95%",
-          borderBottomLeftRadius: 50,
-          borderBottomRightRadius: 50,
-          paddingHorizontal: 20,
-        }}
-      >
-        <Image
-          source={require("../../assets/menu-bars.png")}
-          style={{
-            height: 10,
-            width: 20,
-            marginTop: 50,
-          }}
-        />
         <View
           style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 25,
-            width: "100%",
+            backgroundColor: "#00a46c",
+            height: "95%",
+            borderBottomLeftRadius: 50,
+            borderBottomRightRadius: 50,
+            paddingHorizontal: 20,
           }}
         >
-          <View style={{ width: "50%" }}>
-            <Text
-              style={{
-                fontSize: 22,
-                color: "#FFF",
-                fontWeight: "bold",
-                marginTop: -30,
-                backgroundColor: "transparent"
-              }}
-            >
-              Fruitmark
-            </Text>
-          </View>
-          <View style={{ width: "50%", alignItems: "flex-end" }}>
-            <Image
-              source={require("../../assets/fm-logo.png")}
-              style={{ height: 110, width: 110, marginTop: -30 }}
-            />
-          </View>
-        </View>
-      </View>
-    </SafeAreaView>
-        <View style={{display: 'flex', alignItems: 'center'}}>
-          <CityFilter
-            cities={cities}
-            cityFilter={changeCtg}
-            fruitsCtg={fruitsCtg}
-            active={active}
-            setActive={setActive}
+          <Image
+            source={require("../../assets/menu-bars.png")}
+            style={{
+              height: 10,
+              width: 20,
+              marginTop: 50,
+            }}
           />
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 25,
+              width: "100%",
+            }}
+          >
+            <View style={{ width: "50%" }}>
+              <Text
+                style={{
+                  fontSize: 22,
+                  color: "#FFF",
+                  fontWeight: "bold",
+                  marginTop: -30,
+                  backgroundColor: "transparent",
+                }}
+              >
+                Fruitmark
+              </Text>
+            </View>
+            <View style={{ width: "51%", alignItems: "flex-end" }}>
+              <Image
+                source={require("../../assets/fm-logo.png")}
+                style={{ height: 110, width: 140, bottom: 50, marginBottom: -62 }}
+              />
+            </View>
+          </View>
         </View>
-        <ScrollView>
+      </SafeAreaView>
+      <View style={{ display: "flex", alignItems: "center", backgroundColor: "rgba(0,164,109,0.1)" }}>
+        <CityFilter
+          cities={cities}
+          cityFilter={changeCtg}
+          fruitsCtg={fruitsCtg}
+          active={active}
+          setActive={setActive}
+        />
+      </View>
+      <ScrollView>
         <LinearGradient
-        colors={["rgba(0,164,109,0.3)", "transparent"]}
-        style={{
-          position: "absolute",
-          left: 0,
-          right: 0,
-          height: 100,
-          marginTop: 0,
-          top: 0,
-        }}
-      />
+          colors={["rgba(0,164,109,0.5)", "transparent"]}
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            height: 100,
+            marginTop: 0,
+            top: -50,
+          }}
+        />
         <View>
           {fruitsCtg.length > 0 ? (
             <View style={styles.listContainer}>
@@ -207,6 +192,7 @@ const styles = StyleSheet.create({
     alignContent: "center",
     justifyContent: "center",
     padding: 0,
+    backgroundColor: "rgba(0,164,109,0.1)"
   },
   listContainer: {
     height: "100%",
@@ -214,7 +200,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     alignItems: "flex-start",
-    backgroundColor: "rgba(0,164,109, 0.1)"
+    backgroundColor: "rgba(0,164,109, 0.1)",
   },
   center: {
     justifyContent: "center",
